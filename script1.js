9d164356536e503daa9212c4db3aaec9285b40f9
D. H. <93092744+01DH01@users.noreply.github.com>
2024-10-31 15:53:31 -0400

    Add files via upload
    added login page

---
+++ script1.js
@@ -0,0 +1,9 @@
+// when Login or Sign up button is clicked - transfers to home page
+document.getElementById('btnLogin').onclick = function()
+{
+    window.location.href = "home.html";
+};
+document.getElementById('btnSignup').onclick = function()
+{
+    window.location.href = "home.html";
+};
