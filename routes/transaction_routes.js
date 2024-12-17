83ce390a983ba74f90efc4406504a730a520975b
Maria Fernanda <52012971+mariafbarrera@users.noreply.github.com>
2024-12-17 04:18:01 -0500

    updated files

--- routes/transaction_routes.js
+++ routes/transaction_routes.js
@@ -7,11 +7,13 @@ const {
   getReport,
 } = require("../controllers/transaction_controller");
 
+const { authenticateToken } = require("../middleware/authenticateToken");
 const router = Router();
 
-router.get("/", getTransaction);
-router.post("/save", saveTransaction);
-router.post("/update", updateTransaction);
-router.post("/delete", deleteTransaction);
-router.get("/report", getReport);
+router.get("/", authenticateToken, getTransaction);
+router.post("/save", authenticateToken, saveTransaction);
+router.post("/update", authenticateToken, updateTransaction);
+router.post("/delete", authenticateToken, deleteTransaction);
+router.get("/report", authenticateToken, getReport);
+
 module.exports = router;
